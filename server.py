c32d9996bc0c75a568c6aaf6f6978dd1a4aaac83
Autrin <autrinhakimi@gmail.com>
2025-06-24 16:03:14 -0500

    Add connection handling

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
 import socket
 
-s = socket.socket(); s.bind(("127.0.0.1", 9000)); s.listen(1)
+s=socket.socket(); s.bind(("127.0.0.1", 9000)); s.listen(1)
+conn,_=s.accept(); print(conn.recv(1024))
