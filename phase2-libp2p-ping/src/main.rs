4ee82b4b9c44a39a7aaa8c9e9614c9e8b7333cc7
Autrin <autrinhakimi@gmail.com>
2025-08-03 16:00:47 -0700

    Add Network behaviour

--- phase2-libp2p-ping/src/main.rs
+++ phase2-libp2p-ping/src/main.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 
+use libp2p::{noise, ping, tcp, yamux};
 use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
@@ -8,7 +9,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .with_env_filter(EnvFilter::from_default_env())
         .try_init();
 
-    let mut swarm = libp2p::SwarmBuilder::with_new_identity();
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?
+        .with_behaviour(|_| ping::Behaviour::default())?;
 
     Ok(())
 }
