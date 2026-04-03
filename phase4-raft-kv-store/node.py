73705dc289e07dc98a9812eafc40c61d9b14baec
Autrin <autrinhakimi@gmail.com>
2026-04-03 16:43:50 -0700

    Add some setters and set the leader for all nodes. Create enum State.

--- phase4-raft-kv-store/node.py
+++ phase4-raft-kv-store/node.py
@@ -1,3 +1,4 @@
+
 """
 A 3-node cluster that survives killing one process and still serves reads.
 
@@ -6,6 +7,13 @@ A 3-node cluster that survives killing one process and still serves reads.
 - Expand Raft nodes so the leader accepts SET key value and replicates it to followers.
 
 """
+from enum import Enum
+
+
+class State(Enum):
+  FOLLOWER = "follower"
+  CANDIDATE = "candidate"
+  LEADER = "leader"
 
 class Node:
   def __init__(self, id, state):
@@ -18,6 +26,15 @@ class Node:
 
   def get_id(self):
     return self.id
+  
+  def get_state(self):
+    return self.state
+  
+  def set_state(self, state):
+    self.state = state
+
+  def set_leader(self, leader):
+    self.leader = leader
     
 class Raft():
   def __init__(self, nodes):
@@ -27,7 +44,7 @@ class Raft():
 
   def apply_vote(self):
     for node in self.nodes:
-      if node.state == "follower": # you can vote
+      if node.get_state() == State.FOLLOWER: # you can vote
         voted = node.vote
         self.nodes[voted.get_id()].votes += 1
 
@@ -38,7 +55,13 @@ class Raft():
       if node.votes > votes:
         votes = node.votes
         self.leader = node
+        node.set_state(State.LEADER)
+
+    for node in self.nodes:
+      if node.get_state() != State.LEADER:
+        node.set_leader(self.leader)
 
 def main():
-
+  nodes = [Node(0, State.FOLLOWER), Node(1, State.FOLLOWER), Node(2, State.FOLLOWER), Node(3, State.LEADER)]
+  raft = Raft(nodes)
   return 0
