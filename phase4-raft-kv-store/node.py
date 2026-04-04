c91e3e32334895545932942800cac1500c8742cb
Autrin <autrinhakimi@gmail.com>
2026-04-03 17:49:32 -0700

    Add todos

--- phase4-raft-kv-store/node.py
+++ phase4-raft-kv-store/node.py
@@ -1,10 +1,11 @@
 
 """
-A 3-node cluster that survives killing one process and still serves reads.
+So far a simple Raft implementation that randomly elects a leader every 1-2 seconds that is not distributed and does not handle failures.
+TODO A 3-node cluster that survives killing one process and still serves reads.
 
 - node.py with a timer that "elections" every random 1-2 s.
-- Broadcast "RequestVote" messages over local UDP sockets.
-- Expand Raft nodes so the leader accepts SET key value and replicates it to followers.
+- TODO Broadcast "RequestVote" messages over local UDP sockets.
+- TODO Expand Raft nodes so the leader accepts SET key value and replicates it to followers.
 
 """
 from enum import Enum
